313aef152357b3dbc0213ab4fac0e8411484dcab
Eric Martin Feltham <eric.feltham@aya.yale.edu>
2025-07-07 21:13:28 -0400

    add tests

--- test/standardized_tests.jl
+++ test/standardized_tests.jl
@@ -167,33 +167,6 @@
         println("  StatsModels: $(round(time_ref * 1000, digits=3)) ms")
         println("  Speedup: $(round(time_ref / time_ours, digits=2))x")
     end
-    
-    # @testset "Integration with Margins.jl" begin
-    #     # Test that the ZScoredTerm support enables Margins.jl to work
-    #     using Margins  # Assuming Margins.jl is available
-        
-    #     contrasts = Dict(:x => ZScore(), :y => ZScore())
-        
-    #     f = @formula(outcome ~ x + y + z)
-    #     m = lm(f, df; contrasts=contrasts)
-        
-    #     # This should work without the original MethodError
-    #     @test_nowarn margins(m, :x, df)
-        
-    #     # Test with specific variable
-    #     ame_result = margins(m, :x, df)
-    #     @test ame_result isa MarginsResult
-        
-    #     # The AME should be approximately the coefficient for standardized variable
-    #     coefs = coef(m)
-    #     coef_names = coefnames(m)
-    #     x_coef_idx = findfirst(contains("x"), coef_names)
-        
-    #     if !isnothing(x_coef_idx)
-    #         # For standardized variables, AME should be close to the coefficient
-    #         @test abs(ame_result.effects[:x] - coefs[x_coef_idx]) < 0.1
-    #     end
-    # end
 end
 
 @testset "ZScoredTerm apply_zscore_inplace! function" begin
