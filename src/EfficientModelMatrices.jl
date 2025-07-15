b6aac5111086e547a1c5c1412524ba609c7657cb
Eric Martin Feltham <eric.feltham@aya.yale.edu>
2025-07-14 23:10:54 -0400

    project

--- src/EfficientModelMatrices.jl
+++ src/EfficientModelMatrices.jl
@@ -7,16 +7,18 @@ module EfficientModelMatrices
 using Random # testing?
 
 using StatsModels, GLM, CategoricalArrays, Tables, DataFrames, Random
+
 import MixedModels
+using MixedModels: LinearMixedModel, GeneralizedLinearMixedModel
+
+# Support for StandardizedPredictors.jl
+using StandardizedPredictors: ZScoredTerm
 
 # Include files in dependency order
 include("fixed_helpers.jl")     # No dependencies
 include("evaluators.jl")        # Uses fixed_helpers
 include("generators.jl")        # Uses evaluators + fixed_helpers
 include("testing.jl")
-
-# Support for StandardizedPredictors.jl
-using StandardizedPredictors: ZScoredTerm
 
 # using LinearAlgebra
 
