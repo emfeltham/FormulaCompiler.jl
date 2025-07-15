f57a9a7a852c0fa331f579e6536453129a49e303
Eric Martin Feltham <eric.feltham@aya.yale.edu>
2025-07-14 23:59:54 -0400

    initial tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,13 @@
 
 using Test
 using Revise
+using Random
 using EfficientModelMatrices
 
 @testset "EfficientModelMatrices.jl" begin
     include("initial_tests.jl")
+    # include("benchmark_tests.jl")
+    # OLD:
     # include("main_tests.jl")
     # include("standardized_tests.jl")
 end
