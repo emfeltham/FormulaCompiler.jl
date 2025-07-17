a08e234127c8d538c994d8ae01cda117340dd1d0
Eric Martin Feltham <eric.feltham@aya.yale.edu>
2025-07-17 10:09:23 -0400

    derivative corrections (untested)

--- src/FormulaCompiler.jl
+++ src/FormulaCompiler.jl
@@ -58,7 +58,6 @@ include("derivative_generators.jl")
 include("derivative_modelrow.jl")
 export compute_derivative_evaluator, compute_interaction_derivative_recursive
 export compute_nary_product_derivative, compute_division_derivative, compute_power_derivative
-export is_zero_evaluator
 export marginal_effects!
 
 export ScaledEvaluator, ProductEvaluator
