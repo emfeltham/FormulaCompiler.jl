91aab387c5526d496ca7d800050bb04bcc3a9bda
Eric Martin Feltham <eric.feltham@aya.yale.edu>
2025-08-26 09:07:17 -0400

    testing, reamaining alloc

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,25 +3,15 @@
 # julia --project="." test/runtests.jl > test/tests.txt 2>&1
 
 using Revise
-using Test
-using BenchmarkTools, Profile
-using FormulaCompiler
-using Random
 
-using Statistics
-using DataFrames, Tables, CategoricalArrays
-using StatsModels, StandardizedPredictors
-using MixedModels, GLM
-    
-# Set consistent random seed for reproducible tests
+# Reproducible tests
 Random.seed!(06515)
 
-@testset "FormulaCompiler.jl Tests" begin
-    
+@testset "FormulaCompiler.jl Tests" begin    
     # Core functionality
     include("test_position_mapping.jl") # Position mapping system
     
     # Models
-    include("test_allocations.jl") # performance
-    include("test_models.jl") # correctness
+    include("test_allocations.jl") # Performance
+    include("test_models.jl") # Correctness
 end
